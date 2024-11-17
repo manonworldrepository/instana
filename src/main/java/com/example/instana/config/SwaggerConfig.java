7bf1458e81c596bc367b02228d1295010b023c5b
manonworldrepository <mosta@DESKTOP-E993ETN>
2024-11-17 18:27:54 +0100

    Bugfix for file upload in Swagger

--- src/main/java/com/example/instana/config/SwaggerConfig.java
+++ src/main/java/com/example/instana/config/SwaggerConfig.java
@@ -1,8 +1,12 @@
 package com.example.instana.config;
 
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.RequestBody;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 
 @Configuration
 public class SwaggerConfig {
@@ -12,9 +16,27 @@ public class SwaggerConfig {
         String[] paths = { "/stats/**" };
 
         return GroupedOpenApi
-                .builder()
-                .group("Instana Distributed Tracing")
-                .pathsToMatch(paths)
-                .build();
+            .builder()
+            .group("Instana Distributed Tracing")
+            .pathsToMatch(paths)
+            .build();
+    }
+
+    @Bean
+    public OpenApiCustomizer customiseOpenApi() {
+        return openApi -> openApi.getPaths()
+                .forEach((path, pathItem) -> pathItem.readOperations().forEach(operation -> {
+                    if (path.equals("/stats") && operation.getOperationId() != null && operation.getOperationId().equals("index")) {
+                        RequestBody requestBody = new RequestBody()
+                            .required(true)
+                            .content(new Content().addMediaType("multipart/form-data",
+                                new io.swagger.v3.oas.models.media.MediaType().schema(new Schema<>()
+                                    .type("object")
+                                    .addProperty("file", new Schema<>().type("string").format("binary"))
+                                )
+                            ));
+                        operation.setRequestBody(requestBody);
+                    }
+                }));
     }
 }
